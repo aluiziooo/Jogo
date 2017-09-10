491d67f7a83069d12b71ba9e83da34fdb20e0430
Aluizio Neto <aluiziooo@users.noreply.github.com>
2017-09-10 18:43:10 -0300

    Update Magia.java

--- Jogo/src/Armas/Magia.java
+++ Jogo/src/Armas/Magia.java
@@ -1,7 +1,7 @@
 package Armas;
 
-public class Magia implements ArmaIF{
-	public void usarArma(){
+public class Magia implements ArmaIF{//Usando interface para implementar o metodo usarArma, Sobrescrevendo o metodo, diferente para cada
+	public void usarArma(){      //classe, temos polimorfismo.
 		System.out.println("~Pense em uma magia foda~");
 	}
 }
