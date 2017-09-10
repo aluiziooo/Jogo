c21e72c5710a63267903e3b5671696c889b72faf
Aluizio Neto <aluiziooo@users.noreply.github.com>
2017-09-10 18:52:20 -0300

    Update LutadorSumo.java

--- Jogo/src/Personagens/LutadorSumo.java
+++ Jogo/src/Personagens/LutadorSumo.java
@@ -1,6 +1,8 @@
 package Personagens;
 
-public class LutadorSumo extends Personagem implements Falador, Corredor {
+public class LutadorSumo extends Personagem implements Falador, Corredor {//Herdando os atributos e comportamentos de personagem e implementando
+//Os comportamentos das interfaces Falador e corredor. Como vai ser uma resposta diferente para os metodos das interfaces a cada chamada para
+//a mesma assinatura do metodo, temos polimorfismo.
 	public void desenhar(){
 	
 	}
