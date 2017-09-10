c0b4a5fc540a6c30acfe5a5871f22136167170b5
Aluizio Neto <aluiziooo@users.noreply.github.com>
2017-09-10 18:51:57 -0300

    Update General.java

--- Jogo/src/Personagens/General.java
+++ Jogo/src/Personagens/General.java
@@ -1,7 +1,8 @@
 package Personagens;
 
-public class General extends Personagem implements Falador, Corredor {
-
+public class General extends Personagem implements Falador, Corredor {//Herdando os atributos e comportamentos de personagem e implementando
+//Os comportamentos das interfaces Falador e corredor. Como vai ser uma resposta diferente para os metodos das interfaces a cada chamada para
+//a mesma assinatura do metodo, temos polimorfismo.	
 	public void desenhar(){
 		
 	}
