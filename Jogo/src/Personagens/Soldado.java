cb8bdfeedc5f1a7a3089117010429ae844359dce
Aluizio Neto <aluiziooo@users.noreply.github.com>
2017-09-10 18:53:57 -0300

    Update Soldado.java

--- Jogo/src/Personagens/Soldado.java
+++ Jogo/src/Personagens/Soldado.java
@@ -1,9 +1,14 @@
 package Personagens;
 
-public class Soldado extends Personagem implements Corredor, Falador{
+public class Soldado extends Personagem implements Corredor, Falador{//Herdando os atributos e comportamentos de personagem e implementando
+//Os comportamentos das interfaces Falador e corredor. Como vai ser uma resposta diferente para os metodos das interfaces a cada chamada para
+//a mesma assinatura do metodo, temos polimorfismo.
 	
 	public void desenhar(){
 		
+	}
+	public void correr(){
+		System.out.println("Eu sou um soldado, tenho que correr!!!");
 	}
 	
 	public void falar() {
