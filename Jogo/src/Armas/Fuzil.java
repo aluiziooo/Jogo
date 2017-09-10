3a4a5717644043cf6de7f85f6df0c4dbad0d6a55
Aluizio Neto <aluiziooo@users.noreply.github.com>
2017-09-10 18:42:37 -0300

    Update Fuzil.java

--- Jogo/src/Armas/Fuzil.java
+++ Jogo/src/Armas/Fuzil.java
@@ -1,7 +1,7 @@
 package Armas;
 
-public class Fuzil implements ArmaIF{
-	public void usarArma(){
+public class Fuzil implements ArmaIF{//Usando interface para implementar o metodo usarArma, Sobrescrevendo o metodo, diferente para cada
+	public void usarArma(){      //classe, temos polimorfismo.
 		System.out.println("Rajada");
 	}
 }
