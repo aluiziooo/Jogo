746faa4e6347a0551b89b187947924a1f7f2c8df
Aluizio Neto <aluiziooo@users.noreply.github.com>
2017-09-10 18:39:17 -0300

    Update Faca.java

--- Jogo/src/Armas/Faca.java
+++ Jogo/src/Armas/Faca.java
@@ -1,7 +1,7 @@
 package Armas;
 
-public class Faca implements ArmaIF {
-	public void usarArma(){
+public class Faca implements ArmaIF {//Usando interface para implementar o metodo usarArma, Sobrescrevendo o metodo, diferente para cada
+	public void usarArma(){      //classe, temos polimorfismo.
 		System.out.println("Morreu na faquinha");
 	}
 }
